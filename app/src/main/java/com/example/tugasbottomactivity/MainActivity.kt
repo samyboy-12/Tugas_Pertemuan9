aa44ee0412826c89bad65427971563cfb423f80e
Samuel <samuelardianvalentinosilaban@mail.ugm.ac.id>
2024-10-29 23:25:17 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugasbottomactivity/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.tugasbottomactivity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.tugasbottomactivity.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding){
+            val navController = findNavController(R.id.nav_host_fragment)
+            bottomNavigationView.setupWithNavController(navController)
+
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
